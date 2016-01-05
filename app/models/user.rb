bf8f5be6ae3fc7a78161bcdd57735d27d4875482
kyle kersey <kylelk@comcast.net>
2016-01-05 14:28:16 -0800

    added sign in

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,7 +1,10 @@
 class User < ActiveRecord::Base
   # Include default devise modules. Others available are:
   # :confirmable, :lockable, :timeoutable and :omniauthable
-
   devise :database_authenticatable, :registerable,
          :recoverable, :rememberable, :trackable, :validatable
+
+  validates_length_of :username, :minimum => 3
+  validates_uniqueness_of :username
+
 end
